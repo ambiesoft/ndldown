39684f4da611e5ae31f57530b0f2fa0bf3cec299
ambiesoft <ambiesoft.trueff@gmail.com>
2021-12-06 22:55:05 +0900

    nazo

--- ndldown.js
+++ ndldown.js
@@ -3,17 +3,17 @@ const commandLineArgs = require('command-line-args');
 const ndlfunc = require('./ndlfunc')
 
 function trace(message) {
-    console.log(message)
+  console.log(message)
 }
 
 launchOption = {
-    // executablePath: 
-    headless: false, 
-    slowMo: 50, // typing delay
-    timeout: 60000, 
-    args: [
-      '--incognito'
-    ],
+  // executablePath: 
+  headless: false,
+  slowMo: 50, // typing delay
+  timeout: 60000,
+  args: [
+    '--incognito'
+  ],
 };
 
 
@@ -32,64 +32,108 @@ const optionDefinitions = [
 ];
 const options = commandLineArgs(optionDefinitions);
 
-if(!options.url) {
-    console.error('No url')
-    return
+if (!options.url) {
+  console.error('No url')
+  return
 }
 
 
 async function waitForPageLoaded(page, url) {
-    let tried = 0
-    const wait = 60000
-    if(url && url != page.url()) {
-      await page.goto(url)
-      while(url != page.url()) {
-        trace(`${url} != ${page.url()}`)
-        await page.waitForNavigation({timeout: wait, waitUntil: "domcontentloaded"});
-        if(++tried > 100) {
-          console.error('Can not reach ' + url)
-          return false
-        }
+  let tried = 0
+  const wait = 60000
+  if (url && url != page.url()) {
+    await page.goto(url)
+    while (url != page.url()) {
+      trace(`${url} != ${page.url()}`)
+      await page.waitForNavigation({ timeout: wait, waitUntil: "domcontentloaded" });
+      if (++tried > 100) {
+        console.error('Can not reach ' + url)
+        return false
       }
-    } else {
-      await page.waitForNavigation({timeout: wait, waitUntil: "domcontentloaded"});
     }
-    return true;
+  } else {
+    await page.waitForNavigation({ timeout: wait, waitUntil: "domcontentloaded" });
   }
+  return true;
+}
+
+async function waitForPDFPage(page) {
+  let tried = 0
+  const wait = 60000
+  
+    
+    while (true) {
+      trace(page.url());
+      // await page.waitForNavigation({ timeout: wait, waitUntil: ['networkidle2'] });
+      await page.waitForNavigation();
+      let cont = await page.content();
+      trace(cont);
+      if (++tried > 100) {
+        console.error('can not get PDF page')
+        return false
+      }
+      page.conte
+    }
+  
+  return true;
+}
 
 // -----------------
-(async ()=> {
-    const brow = await pptr.launch(launchOption)    
-    const page = await brow.newPage()
-    trace('New page created')
-    let url = options.url;
-    try {
-        if(!await waitForPageLoaded(page, url)) {
-            return;
-        }
-        trace('Navigated to ' + url);
-
-        const endIndexElem = await page.$('#form-page-control-selection > div.mb-placeholder.mb-pageindex')
-        let endIndex = await endIndexElem.evaluate((node)=>node.innerText)
-        endIndex = endIndex.match(/\d+/g)
-        endIndex = parseInt(endIndex, 10)
-        if(endIndex <= 0) {
-            throw 'EndIndex not set'
-        }
-        trace('EndIndex is ' + endIndex);
-
-        const ranges = ndlfunc.getDownRanges(endIndex)
-
-        // kokokara
-        console.log(ranges)
-
-    } catch(err) {
-        console.error(err)
-    } finally {
-        await brow.close();
+(async () => {
+  const brow = await pptr.launch(launchOption)
+  const page = await brow.newPage()
+  trace('New page created')
+  let url = options.url;
+  try {
+    if (!await waitForPageLoaded(page, url)) {
+      return;
     }
+    trace('Navigated to ' + url);
 
+    const endIndexElem = await page.$('#form-page-control-selection > div.mb-placeholder.mb-pageindex')
+    let endIndex = await endIndexElem.evaluate((node) => node.innerText)
+    endIndex = endIndex.match(/\d+/g)
+    endIndex = parseInt(endIndex, 10)
+    if (endIndex <= 0) {
+      throw 'EndIndex not set'
+    }
+    trace('EndIndex is ' + endIndex);
+
+    const ranges = ndlfunc.getDownRanges(endIndex)
+    for(let i=0 ; i < ranges.length; ++i) {
+      let range = ranges[i];
+      let srange = range[0] + '-' + (range.length >= 2 ? range[1] : '');
+      trace(srange);
+
+      const btnPrint = await page.$('#btn-print');
+      if(!btnPrint) {
+        throw 'btnPrint not found';
+      }
+      await btnPrint.click();
+
+      const inputRange = await page.$('#form-dialog-print > ul > li:nth-child(2) > input[type=text]:nth-child(3)');
+      inputRange.type(srange);
+
+      const btnCreatePDF = await page.$('#btn-print-notinlibrary');
+      // const navigationPromise = page.waitForNavigation();
+      await btnCreatePDF.click();
+
+      // This never ends
+      // await Promise.all([btnCreatePDF.click(), page.waitForNavigation()]);
+      // await navigationPromise;
+
+      let cont = await page.content();
+      trace(cont);
+    }
     
-    
+
+  } catch (err) {
+    console.error(err)
+  } finally {
+    await brow.close();
+  }
+
+
+
 })();
 
--- test.js
+++ test.js
@@ -5,3 +5,11 @@ console.info(v, ndlfunc.getDownRanges(v))
 
 v = 101
 console.info(v, ndlfunc.getDownRanges(v))
+
+
+let ranges = ndlfunc.getDownRanges(269);
+ranges.forEach(range => {
+    console.log(range);
+    let srange = range[0] + '-' + (range.length >= 2 ? range[1] : '');
+    console.log(srange);
+});
